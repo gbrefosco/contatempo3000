c72a25164c692eeb54b67aa2ec6fdeb14bbea2fd
Eduardo da Silva <eduardo.silva@icompsoft.com.br>
2021-06-22 20:39:07 -0300

    Trazendo alterações do repositório

---
+++ contatempo3000-frontend/src/components/login.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './login.css';
+
+export default function Login(){
+    return (
+        <div>
+            <div className="titulo">
+                <h1 className="title">TascheZeit</h1>
+                <h2 className="description">A simple and pratical time-manager <br/> for those who want to be organized</h2>
+            </div>
+            <div className="PLogin">
+                <label className="login_label"><strong>Login</strong></label>
+                <input className="login_input" type="text" placeholder="Type in your login account"></input>
+                <input className="pass_input" type="password" placeholder="Type in your password"></input>
+                <a href="www.google.com">Forgot your password?</a>
+                
+                <button className="continue">Continue</button>
+
+                <p className="signUp">
+                    Don't have an account?
+                    <button>Sign Up</button>
+                </p>
+            </div>
+        </div>
+    );
+}
