5752d27c1f6c1544d67364aa751b0d35477fdf53
gbrefosco <gbrefosco@gmail.com>
2021-06-23 00:56:53 +0000

    cria modelo customer

---
+++ contatempo3000-api/api/models/Customer.js
@@ -0,0 +1,19 @@
+/**
+ * Customer.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    name: {
+      type: 'string',
+      maxLength: 45,
+      required: true
+    }
+  },
+
+};
+
