015bb5a2342f305bca327770c88f45b6be732485
Eduardo da Silva <eduardosilva@ceicom.com.br>
2021-07-25 22:59:37 -0300

    Refeito menu lateral com um design mais chamativo e dinâmico

--- contatempo3000-frontend/src/components/global/sideNav.js
+++ contatempo3000-frontend/src/components/global/sideNav.js
@@ -1,42 +1,69 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-
-import { IconContext } from "react-icons";
-import * as AiIcons from "react-icons/ai";
-
-import { SideNavData } from "./sideNavData";
+import React from "react";
 import "./sideNav.css";
 
+import * as AiIcons from "react-icons/ai";
+
 function SideNavMenu() {
-  const [sidebar, setSidebar] = useState(false);
 
-  const showSidebar = () => setSidebar(!sidebar);
+    function menuBtnChange() {
+        let sidebar = document.querySelector(".sidebar");
+        let closeBtn = document.querySelector("#btn");
+        if (sidebar.classList.contains("open")) {
+            closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+        } else {
+            closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+        }
+    }
 
-  return (
-    <>
-      <IconContext.Provider value={{ color: '#fff' }}>
-        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-          <ul className='nav-menu-items' onClick={showSidebar}>
-            <li className={sidebar ? 'navbar-toggle-on' : 'navbar-toggle-off'}>
-              <Link to='#' className='menu-bars'>
-                <AiIcons.AiOutlineClose />
-              </Link>
-            </li>
-            {SideNavData.map((item, index) => {
-              return (
-                <li key={index} className={item.cName} visible={sidebar}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </IconContext.Provider>
-    </>
-  );
+    function closeBtnHandleClick() {
+        let sidebar = document.querySelector(".sidebar");
+        sidebar.classList.toggle("open");
+        menuBtnChange();
+    }
+
+    return (
+        <>
+            <div class="sidebar" >
+                <div class="logo-details" >
+                    <div class="logo_name" > Hi, TheDukrl </div>
+                    <i class='bx bx-menu' id="btn" onClick={closeBtnHandleClick} >
+                        <div class="line1" ></div>
+                        <div class="line2" ></div>
+                        <div class="line3" ></div>
+                    </i>
+                </div>
+                <ul class="nav-list" >
+                    <li >
+                        <a href="/" >
+                            <i class='bx bx-grid-alt' >
+                                <AiIcons.AiOutlineHome />
+                            </i>
+                            <span class="links_name" > Home </span>
+                        </a>
+                        <span class="tooltip" > Home </span>
+                    </li>
+                    <li >
+                        <a href="/graphics" >
+                            <i class='bx bx-user' >
+                                <AiIcons.AiOutlineProject />
+                            </i>
+                            <span class="links_name" > Graphics </span>
+                        </a>
+                        <span class="tooltip" > Graphics </span>
+                    </li>
+                    <li class="profile" >
+                        <a href="/profile" >
+                            <i class='bx bx-log-out' id="log_out" >
+                                <AiIcons.AiOutlineUser />
+                            </i>
+                            <span class="links_name" > Profile </span>
+                        </a>
+                        <span class="tooltip" > Profile </span>
+                    </li>
+                </ul>
+            </div>
+        </>
+    );
 }
 
 export default SideNavMenu;
